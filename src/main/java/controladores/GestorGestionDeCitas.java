f18787f6eedcb67904773261fe1c69a05dca37ee
Santiago González <santiago.galvez@correounivalle.edu.co>
2022-12-28 11:34:20 -0500

    Algo de refactorizacion

--- src/main/java/controladores/GestorGestionDeCitas.java
+++ src/main/java/controladores/GestorGestionDeCitas.java
@@ -147,15 +147,15 @@ public class GestorGestionDeCitas {
                     JOptionPane.showMessageDialog(vistaGestionCitas, "<html><p style = \" font:12px; \">Por favor ingrese una cédula</p></html>", "Error", JOptionPane.OK_OPTION, UIManager.getIcon("OptionPane.errorIcon"));
                 } else {
                     long cedula = Long.parseLong(cedulaABuscar);
-
+                    
                     if(!almacenamiento.getAfiliados().containsKey(cedula)){
                         JOptionPane.showMessageDialog(vistaGestionCitas, "<html><p style = \" font:12px; \">No se encontró ningún afiliado registrado con esa cédula</p></html>", "Afiliado no encontrado", JOptionPane.OK_OPTION, UIManager.getIcon("OptionPane.errorIcon"));
                     } else {
-                        
                         String motivoCita = (String) JOptionPane.showInputDialog(vistaGestionCitas, "<html><p style = \" font:12px; \">Escoja el motivo de la cita</p></html>", "Lista de servicios", JOptionPane.DEFAULT_OPTION, UIManager.getIcon("OptionPane.questionIcon"), misServicios, misServicios[0]);
+                        
                         if (motivoCita != null){
+                            
                             Iterator i = misMedicos.entrySet().iterator();
-                            ArrayList<String> misMedicos2 = new ArrayList();
                             int cont = 0; //Si lo quitas muestra dos veces el mensaje de la linea 184
                             boolean ventanaCreada = false; //Si lo quitas crea dos ventanas em 178 :v
                             
@@ -165,30 +165,27 @@ public class GestorGestionDeCitas {
 
                                 //Convirtiendo los servicios del médico a String[]
                                 String[] servicioDelMedicoStr = new String[servicioDelMedico.size()];
-                                for (int o= 0; o<servicioDelMedico.size(); o++){
+                                for (int o = 0; o < servicioDelMedico.size(); o++){
                                     String servicio = "";
                                     servicio += servicioDelMedico.get(o);
                                     servicioDelMedicoStr[o] = servicio;
                                 }
                                 
                                 //Verificando si existe un médico con la especialidad del servicio requerido
-                                for (String misServicios2 : servicioDelMedicoStr) {
-                                    if (misServicios2.contains(motivoCita) && ventanaCreada == false) {
-                                        Medico medico = mapa.getValue();
+                                for (String servicioAux : servicioDelMedicoStr) {
+                                    if (servicioAux.contains(motivoCita) && ventanaCreada == false) {
                                         PlantillaCita ventanaAgendarCita = new PlantillaCita("Agendar Citas", "Agendar", motivoCita, cedula, almacenamiento);
                                         vistaGestionCitas.dispose();
-                                        misMedicos2.removeAll(misMedicos2);
                                         ventanaCreada = true;
                                         break; 
-                                    } else if (!misServicios2.equals(motivoCita) && cont < 1){
-                                        JOptionPane.showMessageDialog(vistaGestionCitas, "<html><p style = \" font:12px; \">No se encontró ningún médico que preste ese servicio</p></html>", "Afiliado no encontrado", JOptionPane.OK_OPTION, UIManager.getIcon("OptionPane.errorIcon"));
+                                    } else if (!servicioAux.equals(motivoCita) && cont < 1){
+                                        JOptionPane.showMessageDialog(vistaGestionCitas, "<html><p style = \" font:12px; \">No se encontró ningún médico que preste ese servicio</p></html>", "Médico sin servicio requerido", JOptionPane.OK_OPTION, UIManager.getIcon("OptionPane.errorIcon"));
                                         cont +=1;
                                         break;
                                     }
                                 }
                             }  
                         }
-                        
                     }
                 }
             } catch(NullPointerException np){
