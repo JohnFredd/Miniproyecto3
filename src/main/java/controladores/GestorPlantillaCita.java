c7d738199d7ffe7b8f106b16ed118273dee57e15
Santiago González <santiago.galvez@correounivalle.edu.co>
2022-12-29 14:46:25 -0500

    detector de cambios en fecha

--- src/main/java/controladores/GestorPlantillaCita.java
+++ src/main/java/controladores/GestorPlantillaCita.java
@@ -158,6 +158,7 @@ public class GestorPlantillaCita {
                 }
                 
                 if (e.getButton() == 1){
+                    agregarOyente();
                     medicosDisponibles();
                 }
             }
@@ -174,10 +175,11 @@ public class GestorPlantillaCita {
 
         @Override
         public void itemStateChanged(ItemEvent e) {
-            vistaPlantillaCita.getTxtConsultorio().setText("");
-            vistaPlantillaCita.getBtnAgendar().setEnabled(false);
+            if(e.getSource() == vistaPlantillaCita.getComboMedico()){
+                vistaPlantillaCita.getTxtConsultorio().setText("");
+                vistaPlantillaCita.getBtnAgendar().setEnabled(false);
+            }
         }
-        
     }
     
     public void agendarCita() {
@@ -365,6 +367,24 @@ public class GestorPlantillaCita {
         }
         fechaValida = true;
         return fechaValida;
+    }
+    
+    public void agregarOyente() {
+        vistaPlantillaCita.getDateChooser().addPropertyChangeListener(
+            new java.beans.PropertyChangeListener() {
+
+                @Override
+                public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                    if (!evt.getOldValue().equals(evt.getNewValue())) {
+                        vistaPlantillaCita.limpiarMedicosCombo();
+                        opcionesComboBox = new ArrayList();
+                        vistaPlantillaCita.getTxtConsultorio().setText("");
+                        vistaPlantillaCita.getComboMedico().setEnabled(false);
+                        vistaPlantillaCita.getBtnAsignar().setEnabled(false);
+                        vistaPlantillaCita.getBtnAgendar().setEnabled(false);
+                    }
+                }
+            });
     }
     
     public void asignarReferenciaCita() {
